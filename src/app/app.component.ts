f364ad171bb721f4b84e382ef3253d8b0b76b50b
EdisAnthony <edisanthony@gmail.com>
2022-01-18 08:43:17 -0600

    aplication finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'crypto-angular';
+  encodedText:string=""
+  decodedText:string=""
+
+  encodeText(input:string):void{
+    let result:string = btoa(encodeURI(input))
+    this.encodedText=result
+  }
+
+  decodeText(input:string):void{
+    let result:string = decodeURI(atob(input));
+    this.decodedText=result
+  }
+
+  restartEncoded():void{
+    this.encodedText=""
+  }
+
+  restartDecoded():void{
+    this.decodedText=""
+  }
+
+  copyText(input:string){
+    navigator.clipboard.writeText(input)
+    .then(e=>{
+      alert("Copiado: "+input)
+    })
+  }
+
 }
